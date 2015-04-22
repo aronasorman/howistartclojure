b7a296ea08a4d30b7262d858b0f08e1b7e736a8a
Aron Fyodor Asor <aronasorman@gmail.com>
2015-04-21 21:05:21 -0700

    skeleton project.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to howistartclojure
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/howistartclojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns howistartclojure.core-test
+  (:require [clojure.test :refer :all]
+            [howistartclojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
